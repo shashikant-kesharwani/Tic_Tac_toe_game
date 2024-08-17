d3db1600715c911ce860b261cf8fbe3ac5f3559e
shashikant-kesharwani <rajjukesharwani2002@gmail.com>
2024-08-17 21:25:36 +0530

    add new game

---
+++ HangMan_Game.Designer.vb
@@ -0,0 +1,419 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HangMan_Game
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        btnA = New Button()
+        btnB = New Button()
+        btnC = New Button()
+        btnD = New Button()
+        btnE = New Button()
+        btnF = New Button()
+        btnG = New Button()
+        btnH = New Button()
+        btnI = New Button()
+        btnJ = New Button()
+        btnK = New Button()
+        btnL = New Button()
+        btnM = New Button()
+        btnN = New Button()
+        btnO = New Button()
+        btnP = New Button()
+        btnQ = New Button()
+        btnR = New Button()
+        btnS = New Button()
+        btnT = New Button()
+        btnU = New Button()
+        btnV = New Button()
+        btnW = New Button()
+        btnX = New Button()
+        btnY = New Button()
+        btnZ = New Button()
+        btnstart = New Button()
+        txtresult = New TextBox()
+        Label1 = New Label()
+        btnnext = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(629, 127)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(418, 585)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' btnA
+        ' 
+        btnA.Location = New Point(17, 308)
+        btnA.Name = "btnA"
+        btnA.Size = New Size(48, 41)
+        btnA.TabIndex = 1
+        btnA.Text = "A"
+        btnA.UseVisualStyleBackColor = True
+        ' 
+        ' btnB
+        ' 
+        btnB.Location = New Point(71, 308)
+        btnB.Name = "btnB"
+        btnB.Size = New Size(51, 41)
+        btnB.TabIndex = 2
+        btnB.Text = "B"
+        btnB.UseVisualStyleBackColor = True
+        ' 
+        ' btnC
+        ' 
+        btnC.Location = New Point(137, 308)
+        btnC.Name = "btnC"
+        btnC.Size = New Size(47, 41)
+        btnC.TabIndex = 3
+        btnC.Text = "C"
+        btnC.UseVisualStyleBackColor = True
+        ' 
+        ' btnD
+        ' 
+        btnD.Location = New Point(200, 308)
+        btnD.Name = "btnD"
+        btnD.Size = New Size(47, 41)
+        btnD.TabIndex = 4
+        btnD.Text = "D"
+        btnD.UseVisualStyleBackColor = True
+        ' 
+        ' btnE
+        ' 
+        btnE.Location = New Point(264, 308)
+        btnE.Name = "btnE"
+        btnE.Size = New Size(50, 41)
+        btnE.TabIndex = 5
+        btnE.Text = "E"
+        btnE.UseVisualStyleBackColor = True
+        ' 
+        ' btnF
+        ' 
+        btnF.Location = New Point(329, 308)
+        btnF.Name = "btnF"
+        btnF.Size = New Size(49, 41)
+        btnF.TabIndex = 6
+        btnF.Text = "F"
+        btnF.UseVisualStyleBackColor = True
+        ' 
+        ' btnG
+        ' 
+        btnG.Location = New Point(395, 308)
+        btnG.Name = "btnG"
+        btnG.Size = New Size(45, 41)
+        btnG.TabIndex = 7
+        btnG.Text = "G"
+        btnG.UseVisualStyleBackColor = True
+        ' 
+        ' btnH
+        ' 
+        btnH.Location = New Point(457, 308)
+        btnH.Name = "btnH"
+        btnH.Size = New Size(52, 41)
+        btnH.TabIndex = 8
+        btnH.Text = "H"
+        btnH.UseVisualStyleBackColor = True
+        ' 
+        ' btnI
+        ' 
+        btnI.Location = New Point(17, 384)
+        btnI.Name = "btnI"
+        btnI.Size = New Size(48, 39)
+        btnI.TabIndex = 9
+        btnI.Text = "I"
+        btnI.UseVisualStyleBackColor = True
+        ' 
+        ' btnJ
+        ' 
+        btnJ.Location = New Point(71, 384)
+        btnJ.Name = "btnJ"
+        btnJ.Size = New Size(51, 39)
+        btnJ.TabIndex = 10
+        btnJ.Text = "J"
+        btnJ.UseVisualStyleBackColor = True
+        ' 
+        ' btnK
+        ' 
+        btnK.Location = New Point(137, 384)
+        btnK.Name = "btnK"
+        btnK.Size = New Size(47, 39)
+        btnK.TabIndex = 11
+        btnK.Text = "K"
+        btnK.UseVisualStyleBackColor = True
+        ' 
+        ' btnL
+        ' 
+        btnL.Location = New Point(200, 384)
+        btnL.Name = "btnL"
+        btnL.Size = New Size(47, 39)
+        btnL.TabIndex = 12
+        btnL.Text = "L"
+        btnL.UseVisualStyleBackColor = True
+        ' 
+        ' btnM
+        ' 
+        btnM.Location = New Point(264, 384)
+        btnM.Name = "btnM"
+        btnM.Size = New Size(50, 39)
+        btnM.TabIndex = 13
+        btnM.Text = "M"
+        btnM.UseVisualStyleBackColor = True
+        ' 
+        ' btnN
+        ' 
+        btnN.Location = New Point(329, 384)
+        btnN.Name = "btnN"
+        btnN.Size = New Size(49, 39)
+        btnN.TabIndex = 14
+        btnN.Text = "N"
+        btnN.UseVisualStyleBackColor = True
+        ' 
+        ' btnO
+        ' 
+        btnO.Location = New Point(395, 384)
+        btnO.Name = "btnO"
+        btnO.Size = New Size(45, 39)
+        btnO.TabIndex = 15
+        btnO.Text = "O"
+        btnO.UseVisualStyleBackColor = True
+        ' 
+        ' btnP
+        ' 
+        btnP.Location = New Point(457, 384)
+        btnP.Name = "btnP"
+        btnP.Size = New Size(52, 39)
+        btnP.TabIndex = 16
+        btnP.Text = "P"
+        btnP.UseVisualStyleBackColor = True
+        ' 
+        ' btnQ
+        ' 
+        btnQ.Location = New Point(17, 462)
+        btnQ.Name = "btnQ"
+        btnQ.Size = New Size(48, 39)
+        btnQ.TabIndex = 17
+        btnQ.Text = "Q"
+        btnQ.UseVisualStyleBackColor = True
+        ' 
+        ' btnR
+        ' 
+        btnR.Location = New Point(71, 462)
+        btnR.Name = "btnR"
+        btnR.Size = New Size(51, 39)
+        btnR.TabIndex = 18
+        btnR.Text = "R"
+        btnR.UseVisualStyleBackColor = True
+        ' 
+        ' btnS
+        ' 
+        btnS.Location = New Point(137, 462)
+        btnS.Name = "btnS"
+        btnS.Size = New Size(47, 39)
+        btnS.TabIndex = 19
+        btnS.Text = "S"
+        btnS.UseVisualStyleBackColor = True
+        ' 
+        ' btnT
+        ' 
+        btnT.Location = New Point(200, 462)
+        btnT.Name = "btnT"
+        btnT.Size = New Size(47, 39)
+        btnT.TabIndex = 20
+        btnT.Text = "T"
+        btnT.UseVisualStyleBackColor = True
+        ' 
+        ' btnU
+        ' 
+        btnU.Location = New Point(264, 462)
+        btnU.Name = "btnU"
+        btnU.Size = New Size(50, 39)
+        btnU.TabIndex = 21
+        btnU.Text = "U"
+        btnU.UseVisualStyleBackColor = True
+        ' 
+        ' btnV
+        ' 
+        btnV.Location = New Point(329, 462)
+        btnV.Name = "btnV"
+        btnV.Size = New Size(49, 39)
+        btnV.TabIndex = 22
+        btnV.Text = "V"
+        btnV.UseVisualStyleBackColor = True
+        ' 
+        ' btnW
+        ' 
+        btnW.Location = New Point(393, 459)
+        btnW.Name = "btnW"
+        btnW.Size = New Size(47, 42)
+        btnW.TabIndex = 23
+        btnW.Text = "W"
+        btnW.UseVisualStyleBackColor = True
+        ' 
+        ' btnX
+        ' 
+        btnX.Location = New Point(457, 459)
+        btnX.Name = "btnX"
+        btnX.Size = New Size(52, 42)
+        btnX.TabIndex = 24
+        btnX.Text = "X"
+        btnX.UseVisualStyleBackColor = True
+        ' 
+        ' btnY
+        ' 
+        btnY.Location = New Point(200, 529)
+        btnY.Name = "btnY"
+        btnY.Size = New Size(47, 45)
+        btnY.TabIndex = 25
+        btnY.Text = "Y"
+        btnY.UseVisualStyleBackColor = True
+        ' 
+        ' btnZ
+        ' 
+        btnZ.Location = New Point(264, 529)
+        btnZ.Name = "btnZ"
+        btnZ.Size = New Size(50, 45)
+        btnZ.TabIndex = 26
+        btnZ.Text = "Z"
+        btnZ.UseVisualStyleBackColor = True
+        ' 
+        ' btnstart
+        ' 
+        btnstart.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(255))
+        btnstart.Location = New Point(3, 122)
+        btnstart.Name = "btnstart"
+        btnstart.Size = New Size(204, 59)
+        btnstart.TabIndex = 27
+        btnstart.Text = "Start Game"
+        btnstart.UseVisualStyleBackColor = False
+        ' 
+        ' txtresult
+        ' 
+        txtresult.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        txtresult.Location = New Point(12, 219)
+        txtresult.Multiline = True
+        txtresult.Name = "txtresult"
+        txtresult.ReadOnly = True
+        txtresult.Size = New Size(522, 57)
+        txtresult.TabIndex = 28
+        ' 
+        ' Label1
+        ' 
+        Label1.BackColor = Color.Yellow
+        Label1.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.ButtonShadow
+        Label1.Location = New Point(321, 21)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(393, 64)
+        Label1.TabIndex = 29
+        Label1.Text = "Fruits Name"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' btnnext
+        ' 
+        btnnext.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        btnnext.Location = New Point(321, 122)
+        btnnext.Name = "btnnext"
+        btnnext.Size = New Size(188, 59)
+        btnnext.TabIndex = 30
+        btnnext.Text = "Next"
+        btnnext.UseVisualStyleBackColor = False
+        ' 
+        ' HangMan_Game
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 28F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1076, 733)
+        Controls.Add(btnnext)
+        Controls.Add(Label1)
+        Controls.Add(txtresult)
+        Controls.Add(btnstart)
+        Controls.Add(btnZ)
+        Controls.Add(btnY)
+        Controls.Add(btnX)
+        Controls.Add(btnW)
+        Controls.Add(btnV)
+        Controls.Add(btnU)
+        Controls.Add(btnT)
+        Controls.Add(btnS)
+        Controls.Add(btnR)
+        Controls.Add(btnQ)
+        Controls.Add(btnP)
+        Controls.Add(btnO)
+        Controls.Add(btnN)
+        Controls.Add(btnM)
+        Controls.Add(btnL)
+        Controls.Add(btnK)
+        Controls.Add(btnJ)
+        Controls.Add(btnI)
+        Controls.Add(btnH)
+        Controls.Add(btnG)
+        Controls.Add(btnF)
+        Controls.Add(btnE)
+        Controls.Add(btnD)
+        Controls.Add(btnC)
+        Controls.Add(btnB)
+        Controls.Add(btnA)
+        Controls.Add(PictureBox1)
+        Font = New Font("Segoe UI", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4)
+        Name = "HangMan_Game"
+        Text = "HangMan_Game"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnA As Button
+    Friend WithEvents btnB As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnD As Button
+    Friend WithEvents btnE As Button
+    Friend WithEvents btnF As Button
+    Friend WithEvents btnG As Button
+    Friend WithEvents btnH As Button
+    Friend WithEvents btnI As Button
+    Friend WithEvents btnJ As Button
+    Friend WithEvents btnK As Button
+    Friend WithEvents btnL As Button
+    Friend WithEvents btnM As Button
+    Friend WithEvents btnN As Button
+    Friend WithEvents btnO As Button
+    Friend WithEvents btnP As Button
+    Friend WithEvents btnQ As Button
+    Friend WithEvents btnR As Button
+    Friend WithEvents btnS As Button
+    Friend WithEvents btnT As Button
+    Friend WithEvents btnU As Button
+    Friend WithEvents btnV As Button
+    Friend WithEvents btnW As Button
+    Friend WithEvents btnX As Button
+    Friend WithEvents btnY As Button
+    Friend WithEvents btnZ As Button
+    Friend WithEvents btnstart As Button
+    Friend WithEvents txtresult As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnnext As Button
+End Class
---
+++ HangMan_Game.vb
@@ -0,0 +1,482 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Public Class HangMan_Game
+    Dim str As String
+    Dim i, j As Integer
+    Dim char1 As Char
+    Dim n As Integer = 0
+    Dim nextf As Integer
+    Private Sub btnstart_Click(sender As Object, e As EventArgs) Handles btnstart.Click
+        txtresult.Clear()
+        PictureBox1.Image = My.Resources.hang0
+        str = "Apple"
+        nextf = 1
+        str = str.ToUpper()
+        txtresult.Text = New String("-"c, str.Length) ' Initialize with dashes
+
+    End Sub
+    Sub hang(num As Integer)
+        If n = 1 Then
+            PictureBox1.Image = My.Resources.hang1
+        ElseIf n = 2 Then
+            PictureBox1.Image = My.Resources.hang2
+        ElseIf n = 3 Then
+            PictureBox1.Image = My.Resources.hang3
+        ElseIf n = 4 Then
+            PictureBox1.Image = My.Resources.hang4
+        ElseIf n = 5 Then
+            PictureBox1.Image = My.Resources.hang5
+        ElseIf n = 6 Then
+            PictureBox1.Image = My.Resources.hang6
+            MessageBox.Show("You lose the Game !!!", "SICS")
+            txtresult.Clear()
+            PictureBox1.Image = My.Resources.hang0
+            str = "Apple"
+            nextf = 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        End If
+    End Sub
+
+    Sub change(str1 As Char, ind As Integer)
+        Dim strresult As String = txtresult.Text
+        Dim charArray As Char() = strresult.ToCharArray() ' Convert the string to a character array
+        charArray(ind) = str1 ' Replace the character at the specific index
+        txtresult.Text = New String(charArray) ' Convert the character array back to a string and update the text
+    End Sub
+
+    Private Sub btnA_Click(sender As Object, e As EventArgs) Handles btnA.Click
+        char1 = "A"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+
+    End Sub
+
+    Private Sub btnB_Click(sender As Object, e As EventArgs) Handles btnB.Click
+        j = 0
+        char1 = "B"
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        char1 = "C"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnD_Click(sender As Object, e As EventArgs) Handles btnD.Click
+        char1 = "D"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnE_Click(sender As Object, e As EventArgs) Handles btnE.Click
+        char1 = "E"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnF_Click(sender As Object, e As EventArgs) Handles btnF.Click
+        char1 = "F"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnG_Click(sender As Object, e As EventArgs) Handles btnG.Click
+        char1 = "G"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnH_Click(sender As Object, e As EventArgs) Handles btnH.Click
+        char1 = "H"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnI_Click(sender As Object, e As EventArgs) Handles btnI.Click
+        char1 = "I"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnJ_Click(sender As Object, e As EventArgs) Handles btnJ.Click
+        char1 = "J"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnK_Click(sender As Object, e As EventArgs) Handles btnK.Click
+        char1 = "K"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnL_Click(sender As Object, e As EventArgs) Handles btnL.Click
+        char1 = "L"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnM_Click(sender As Object, e As EventArgs) Handles btnM.Click
+        char1 = "M"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnN_Click(sender As Object, e As EventArgs) Handles btnN.Click
+        char1 = "N"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnO_Click(sender As Object, e As EventArgs) Handles btnO.Click
+        char1 = "O"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnP_Click(sender As Object, e As EventArgs) Handles btnP.Click
+        char1 = "P"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnQ_Click(sender As Object, e As EventArgs) Handles btnQ.Click
+        char1 = "Q"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnR_Click(sender As Object, e As EventArgs) Handles btnR.Click
+        char1 = "R"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnS_Click(sender As Object, e As EventArgs) Handles btnS.Click
+        char1 = "S"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnT_Click(sender As Object, e As EventArgs) Handles btnT.Click
+        char1 = "T"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnU_Click(sender As Object, e As EventArgs) Handles btnU.Click
+        char1 = "U"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnV_Click(sender As Object, e As EventArgs) Handles btnV.Click
+        char1 = "V"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnW_Click(sender As Object, e As EventArgs) Handles btnW.Click
+        char1 = "W"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnX_Click(sender As Object, e As EventArgs) Handles btnX.Click
+        char1 = "X"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnY_Click(sender As Object, e As EventArgs) Handles btnY.Click
+        char1 = "Y"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub btnZ_Click(sender As Object, e As EventArgs) Handles btnZ.Click
+        char1 = "Z"
+        j = 0
+        For i = 0 To str.Length - 1
+            If str.Chars(i) = char1 Then
+                change(char1, i)
+                j = 1
+            End If
+        Next
+        If j = 0 Then
+            n += 1
+            hang(n)
+        End If
+    End Sub
+
+    Private Sub HangMan_Game_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.Image = My.Resources.hang0
+    End Sub
+
+    Private Sub btnnext_Click(sender As Object, e As EventArgs) Handles btnnext.Click
+        If nextf = 1 Then
+            str = "banana"
+            nextf += 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        ElseIf nextf = 2 Then
+            str = "Grapes"
+            nextf += 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        ElseIf nextf = 3 Then
+            str = "mango"
+            nextf += 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        ElseIf nextf = 4 Then
+            str = "orange"
+            nextf += 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        ElseIf nextf = 5 Then
+            str = "fig"
+            nextf += 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        ElseIf nextf = 6 Then
+            str = "coconut"
+            nextf += 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        ElseIf nextf = 7 Then
+            str = "pomegrante"
+            nextf += 1
+            str = str.ToUpper()
+            txtresult.Text = New String("-"c, str.Length)
+        End If
+
+    End Sub
+End Class
